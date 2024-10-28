6e5b4971e68a4e56554bd01d93908cd48d4d4546
mjchoi <dunghasd@gmail.com>
2024-10-28 19:59:26 +0900

    HTTP 서버3 - 기능 추가

--- src/was/v3/HttpRequestHandlerV3.java
+++ src/was/v3/HttpRequestHandlerV3.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static util.MyLogger.log;
@@ -46,7 +48,7 @@ public class HttpRequestHandlerV3 implements Runnable {
             } else if (requestString.startsWith("GET /site2")) {
                 site2(writer);
             } else if (requestString.startsWith("GET /search")) {
-                search(writer);
+                search(writer, requestString);
             } else if (requestString.startsWith("GET / ")) {
                 home(writer);
             } else {
@@ -58,13 +60,36 @@ public class HttpRequestHandlerV3 implements Runnable {
     }
 
     private void site1(PrintWriter writer) {
-
+        writer.println("HTTP/1.1 200 OK");
+        writer.println("Content-Type: text/html; charset=utf-8");
+        writer.println();
+        writer.println("<h1>site1</h1>");
+        writer.flush();
     }
 
     private void site2(PrintWriter writer) {
+        writer.println("HTTP/1.1 200 OK");
+        writer.println("Content-Type: text/html; charset=utf-8");
+        writer.println();
+        writer.println("<h1>site2</h1>");
+        writer.flush();
     }
 
-    private void search(PrintWriter writer) {
+    private void search(PrintWriter writer, String requestString) {
+
+        int startIndex = requestString.indexOf("q=");
+        int endIndex = requestString.indexOf(" ", startIndex + 2);
+        String query = requestString.substring(startIndex + 2, endIndex);
+        String decode = URLDecoder.decode(query, UTF_8);
+        writer.println("HTTP/1.1 200 OK");
+        writer.println("Content-Type: text/html; charset=utf-8");
+        writer.println();
+        writer.println("<h1>Search</h1>");
+        writer.println("<ul>");
+        writer.println("<li>query: " + query + "</li>");
+        writer.println("<li>decode: " + decode + "</li>");
+        writer.println("</ul>");
+        writer.flush();
     }
 
     private void home(PrintWriter writer) {
@@ -81,6 +106,11 @@ public class HttpRequestHandlerV3 implements Runnable {
     }
 
     private void notFound(PrintWriter writer) {
+        writer.println("HTTP/1.1 404 Not Found");
+        writer.println("Content-Type: text/html; charset=utf-8");
+        writer.println();
+        writer.println("<h1>404 페이지를 찾을 수 없습니다.</h1>");
+        writer.flush();
     }
 
     private static String requestToString(BufferedReader reader) throws IOException {
